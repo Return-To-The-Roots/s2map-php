941979e64e93f69a8d5185633fd261e08eb44f1c
FloSoft <FloSoft@94216872-fc00-4243-93dc-8a54330ecf12>
2011-01-18 09:02:11 +0000

    copyright

--- map.php
+++ map.php
@@ -1,5 +1,24 @@
 <?php
 
+// $Id$
+//
+// Copyright (c) 2005 - 2010 Settlers Freaks (sf-team at siedler25.org)
+//
+// This file is part of Return To The Roots.
+//
+// Return To The Roots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Return To The Roots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+
 require_once("libs/s2map/s2map.php");
 
 if(isset($_GET['map']))
@@ -12,7 +31,7 @@ if(isset($_GET['map']))
 }
 
 
-$map = new s2map("/www/ra-doersch.de/nightly/maps/".$map);
+$map = new s2map("maps/".$map);
 
 header("Expires: Mon, 01 Jul 1990 00:00:00 GMT"); 
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") ." GMT"); 
